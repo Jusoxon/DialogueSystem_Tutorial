f8aef67db7a378702a95542630f604bf890e9bae
Junsoo Lee <xdcv154@gmail.com>
2019-07-18 10:11:56 +0900

    end
    end

---
+++ DialogueSystem_Tuotorial/Assets/02_Scripts/LJS/DialogueMng.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueMng : MonoBehaviour
+{ 
+    #region SINGLETON
+    public static DialogueMng Ins;
+    private void Awake()
+    {
+        Ins = this;
+    }
+    #endregion
+
+    public Text nameTxt;
+    public Text dialogueTxt;
+
+    public Queue<string> qSetence = new Queue<string>();
+
+    public void OnClickNextBtn()
+    {
+        if(qSetence.Count <= 0)
+        {
+            Debug.Log("End Dialogue");
+        }
+        dialogueTxt.text = qSetence.Dequeue();
+    }
+
+
+}
---
+++ DialogueSystem_Tuotorial/Assets/02_Scripts/LJS/NPC.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public void OnClickNPCBtn()
+    {
+        DialogueMng.Ins.nameTxt.text = dialogue.npcName;
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            DialogueMng.Ins.qSetence.Enqueue(sentence);
+        }
+
+        DialogueMng.Ins.dialogueTxt.text = DialogueMng.Ins.qSetence.Dequeue();
+    }
+}
